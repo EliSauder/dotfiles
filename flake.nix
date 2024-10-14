dbee8be5b7d49ce5569d706deecc7e43b0d6ae0e
EliSauder <24995216+EliSauder@users.noreply.github.com>
2024-10-14 01:01:41 -0700

    feat: maybe multisystem flake for home manager

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+    description = "Home manager flake for esauder system";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = {self, nixpktd, home-manager, flake-utils }:
+        flake-utils.lib.eachDefaultSystem (system: 
+    let 
+        pkgs = import nixpkgs { 
+            inherit system; 
+        };
+        homeManager = pkgs.home-manager;
+        defaultPackages = home-manager.defaultPackage.system;
+    in {
+        defaultPackage.system = defaultPackages;
+
+        homeConfigurations = {
+            "esauder" = homeManager.lib.homeManagerConfiguration {
+                pkgs = pkgs;
+                modules = [ ./home.nix ]
+            };
+        };
+    });
+}
