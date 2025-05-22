32794c826824d63ddbfc36983dd6edde434fb2c3
EliSauder <24995216+EliSauder@users.noreply.github.com>
2025-05-22 16:55:50 -0700

    feat: add dadbod nvim plugin

---
+++ programs/neovim/plugins/dadbod.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }:
+{
+  programs.nixvim.plugins = {
+    vim-dadbod = {
+      enable = true;
+    };
+    vim-dadbod-completion = {
+      enable = true;
+    };
+    vim-dadbod-ui = {
+      enable = true;
+    };
+  };
+}
