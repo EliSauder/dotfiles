796dce8710a4c42e8b4b3e0b0069a5a017d530a8
EliSauder <24995216+EliSauder@users.noreply.github.com>
2025-06-05 15:42:47 -0700

    chroe: update git/default.nix fmt

--- programs/git/default.nix
+++ programs/git/default.nix
@@ -1,44 +1,50 @@
-{config, pkgs, lib, ... }: 
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 let
-    cfg = config.prog.git;
-in {
-    options.prog = {
-        git.enable = lib.mkEnableOption "Enable git";
-        git.editor = lib.mkOption {
-            type = with lib.types; uniq str; 
+  cfg = config.prog.git;
+in
+{
+  options.prog = {
+    git.enable = lib.mkEnableOption "Enable git";
+    git.editor = lib.mkOption {
+      type = with lib.types; uniq str;
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = [
+      pkgs.difftastic
+    ];
+
+    programs.git = {
+      enable = true;
+      userName = "EliSauder";
+      userEmail = "24995216+EliSauder@users.noreply.github.com";
+      lfs.enable = true;
+      signing = {
+        key = "${config.home.homeDirectory}/.ssh/git_ed25519";
+        signByDefault = true;
+      };
+      extraConfig = {
+        gpg = {
+          format = "ssh";
         };
+        core = {
+          editor = "${cfg.editor}";
+        };
+      };
+      maintenance.enable = true;
+      aliases = {
+        s = "status";
+        co = "checkout";
+        cob = "checkout -b";
+      };
+      difftastic.enable = true;
     };
 
-    config = lib.mkIf cfg.enable {
-        home.packages = [
-            pkgs.difftastic
-        ];
-
-        programs.git = {
-          enable = true;
-          userName = "EliSauder";
-          userEmail = "24995216+EliSauder@users.noreply.github.com";
-          lfs.enable = true;
-          signing = {
-              key = "${config.home.homeDirectory}/.ssh/git_ed25519";
-              signByDefault = true;
-          };
-          extraConfig = {
-              gpg = {
-                  format = "ssh";
-              };
-              core = {
-                  editor = "${cfg.editor}";
-              };
-          };
-          maintenance.enable = true;
-          aliases = {
-            s = "status";
-            co = "checkout";
-            cob = "checkout -b";
-          };
-          difftastic.enable = true;
-        };
-
-    };
+  };
 }
