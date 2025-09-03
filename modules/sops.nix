86c2afba7def9820059ff019a6ef10e0b814260a
EliSauder <24995216+EliSauder@users.noreply.github.com>
2025-09-03 16:41:05 -0700

    fix: correct sops key path

--- modules/sops.nix
+++ modules/sops.nix
@@ -14,10 +14,10 @@
   sops = {
     defaultSopsFile = ./../secrets/secrets.yaml;
     defaultSopsFormat = "yaml";
-    age.keyFile = "${config.home.homeDirectory}/.config/sops/keys.txt";
+    age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
 
-    secrets.apps.spotify.id = { };
-    secrets.apps.spotify.secret = { };
+    secrets."apps/spotify/client_id" = { };
+    secrets."apps/spotify/client_secret" = { };
   };
 
 }
