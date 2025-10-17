3091b6fd4d6067a633ffa8a192deb7ff7a166e35
EliSauder <24995216+EliSauder@users.noreply.github.com>
2025-10-16 19:32:36 -0700

    fix: use base git worktree switcher

--- modules/programs/git-worktree-switcher/default.nix
+++ modules/programs/git-worktree-switcher/default.nix
@@ -6,32 +6,32 @@
 }:
 let
   cfg = config.prog.gitws;
-  package = (
-    pkgs.git-worktree-switcher.overrideAttrs (
-      finalAttrs: previousAtts: {
-        pname = "git-worktree-switcher-patch";
-        version = "0.2.5";
-        src = pkgs.fetchFromGitHub {
-          owner = "EliSauder";
-          repo = "git-worktree-switcher";
-          rev = "a90315c8637e32377abdf6230240e72a8e5471e8";
-          tag = null;
-          hash = "sha256-XMrdnbsk+N1CHpb8WzN/61uZtb1elPU829EuShRil74=";
-        };
-      }
-    )
-  );
+  #package = (
+  #  pkgs.git-worktree-switcher.overrideAttrs (
+  #    finalAttrs: previousAtts: {
+  #      pname = "git-worktree-switcher-patch";
+  #      version = "0.2.5";
+  #      src = pkgs.fetchFromGitHub {
+  #        owner = "EliSauder";
+  #        repo = "git-worktree-switcher";
+  #        rev = "a90315c8637e32377abdf6230240e72a8e5471e8";
+  #        tag = null;
+  #        hash = "sha256-XMrdnbsk+N1CHpb8WzN/61uZtb1elPU829EuShRil74=";
+  #      };
+  #    }
+  #  )
+  #);
 
-  initScript =
-    shell:
-    if (shell == "fish") then
-      ''
-        ${lib.getExe package} init ${shell} | source
-      ''
-    else
-      ''
-        eval "$(${lib.getExe package} init ${shell})"
-      '';
+  #initScript =
+  #  shell:
+  #  if (shell == "fish") then
+  #    ''
+  #      ${lib.getExe package} init ${shell} | source
+  #    ''
+  #  else
+  #    ''
+  #      eval "$(${lib.getExe package} init ${shell})"
+  #    '';
 in
 {
   options.prog = {
@@ -39,12 +39,18 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    home.packages = [
-      package
-    ];
+    programs.git-worktree-switcher = {
+      enable = true;
+      enableBashIntegration = true;
+      enableFishIntegration = true;
+      enableZshIntegration = true;
+    };
+    #home.packages = [
+    #  package
+    #];
 
-    programs.bash.initExtra = initScript "bash";
-    programs.fish.interactiveShellInit = initScript "fish";
-    programs.zsh.initContent = initScript "zsh";
+    #programs.bash.initExtra = initScript "bash";
+    #programs.fish.interactiveShellInit = initScript "fish";
+    #programs.zsh.initContent = initScript "zsh";
   };
 }
