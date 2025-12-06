987a93ec9637f596543e2eac8e0fe3f8dfceaa78
EliSauder <24995216+EliSauder@users.noreply.github.com>
2025-12-06 01:03:56 -0800

    fix: use new commits from qutebrowser fido branch

--- overlays/qutebrowser-dev.nix
+++ overlays/qutebrowser-dev.nix
@@ -10,10 +10,10 @@
       qutebrowser-dev = prev.qutebrowser.overrideAttrs (oldAttrs: {
         src = pkgs.fetchgit {
           url = "https://github.com/coderkun/qutebrowser.git";
-          rev = "4b38ef7a15f7a02a857667c8dbbc3c94ab915b50";
+          rev = "62d5407d2f33fe702a9f90620c21e5f68d996510";
           branchName = "issue-8533-fido2-user-verification";
-          #sha256 = "sha256-8jiGbk13Vy5wBEtudbpv0okOv7gVTwGKtL85sDs78Lc=";
-          sha256 = "sha256-Uv5zQvmkfdib5j+y5nRw1VAqHnVNgVR58Mtxm5iXCOM=";
+          sha256 = "sha256-5PFvAqgeJ6Y+AiA4LVHPYgbhWmpTEHjnqzjplNLyG28=";
+          #sha256 = "sha256-Uv5zQvmkfdib5j+y5nRw1VAqHnVNgVR58Mtxm5iXCOM=";
         };
       });
     })
