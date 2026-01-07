19bfc5e9c5a13f7ea593e0c36e0c4c0ed7ad63ec
EliSauder <24995216+EliSauder@users.noreply.github.com>
2026-01-06 18:48:18 -0800

    fix: some idrive stuff

--- overlays/idrive.nix
+++ overlays/idrive.nix
@@ -7,41 +7,52 @@
 {
   nixpkgs.overlays = [
     (final: prev: {
-      idrive = pkgs.stdenv.mkDerivation {
+      idrive = pkgs.stdenv.mkDerivation rec {
         name = "idrive";
         src = pkgs.fetchurl {
           url = "https://www.idrivedownloads.com/downloads/linux/linux-desktop/IDriveForLinux.deb?ctag=07192025";
           #hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
           hash = "sha256-LijFYYxJbV3JJGN0slvKbzDOLpBjPW91eqI5CJ9uvDI=";
         };
-        phases = [
-          "unpackPhase"
-          "installPhase"
-        ];
 
         nativeBuildInputs = [
+          pkgs.autoPatchelfHook
+          pkgs.makeWrapper
           pkgs.dpkg
         ];
 
         unpackPhase = ''
+          runHook preUnpack
           dpkg-deb -R "$src" idrive
+          runHook postUnpack
         '';
 
         installPhase = ''
+          runHook preInstall
           mkdir -p "$out/"
           cp -r "idrive/opt" "$out/"
-          cp -r "idrive/usr" "$out/"
+          cp -r "idrive/usr/share" "$out/"
 
           #"$out/opt/IDriveForLinux/resources/app.asar.unpacked/IdriveForLinux/idriveforlinux.bin" --install
 
           #chown root:root "$out/opt/IDriveForLinux/chrome-sandbox"
           #chmod 4755 "$out/opt/IDriveForLinux/chrome-sandbox"
 
-          mkdir -p "$out/usr/local/bin"
-          ln -s "$out/opt/IDriveForLinux/idriveforlinux" "$out/usr/local/bin/idriveforlinux"
+          mkdir -p "$out/bin"
+          ln -s "$out/opt/IDriveForLinux/idriveforlinux" "$out/bin/idriveforlinux"
 
           cat "idrive/DEBIAN/control" | grep Version | awk -F' ' '{print $2}' > "$out/opt/IDriveForLinux/AppVersion"
+
+          substituteInPlace "$out/share/applications/idriveforlinux.desktop" --replace "/opt/IDriveForLinux" "$out/opt/IDriveForLinux"
+
+          runHook postInstall
         '';
+
+        #postFixup = ''
+        #  wrapProgram $out/bin/idriveforlinux \
+        #    --prefix LD_LIBRARY_PATH : "${stdenv.lib.makeLibraryPath buildInputs}" \
+        #    --set MY_APP_HOME "$out/opt/IDriveForLinux"
+        #'';
 
         buildInputs = [
           pkgs.nss
@@ -51,6 +62,9 @@
           pkgs.cron
           pkgs.glibc
           pkgs.libappindicator
+          pkgs.libdrm
+          pkgs.libgbm
+          pkgs.alsa-lib
         ];
       };
     })
