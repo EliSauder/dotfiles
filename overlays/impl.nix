76c7593b9ff4129c83956ced54f8ac0b546b3d5c
EliSauder <24995216+EliSauder@users.noreply.github.com>
2026-01-09 12:41:36 -0800

    feat: use fork of impl

--- overlays/impl.nix
+++ overlays/impl.nix
@@ -13,11 +13,11 @@
           repo = pname;
           rev = version;
           #sha256 = "sha256-8jiGbk13Vy5wBEtudbpv0okOv7gVTwGKtL85sDs78Lc=";
-          sha256 = "sha256-ANKUwtIQQKKzo1u6RAY8uIKG5EKpuZa1X/fI379KazY=";
+          sha256 = "sha256-o9fyKc9QfZJTGW7mjnCiRL7kGouF+YdCUcaX5dXDXeQ=";
         };
 
         #cargoHash = "sha256-LU1eaH7XZFOvZtHJhsBptQKckXK5xc9rbiWdGvampTE=";
-        vendorHash = "sha256-7fjIqmk1A7uS16w8HD1+YOolHWiw5yKrLsB65I05qNI=";
+        vendorHash = "sha256-vTqDoM/LK5SHkayLKYig+tCrXLelOoILmQGCxlTWHog=";
       };
     })
   ];
