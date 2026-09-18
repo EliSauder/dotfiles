ce8d8da57170e87fff32bdd6f22383beb6527b9a
EliSauder <24995216+EliSauder@users.noreply.github.com>
2026-09-18 10:34:15 -0700

    build: reduce cores in ubuntu build

--- platforms/ubuntu/home.nix
+++ platforms/ubuntu/home.nix
@@ -39,7 +39,7 @@ in
     ../../modules
   ];
 
-  programs.fish.functions.homebuild = "home-manager switch --flake \"${config.home.homeDirectory}/.dotfiles#${username}-ubuntu\" --cores 6 --impure $argv";
+  programs.fish.functions.homebuild = "home-manager switch --flake \"${config.home.homeDirectory}/.dotfiles#${username}-ubuntu\" --cores 3 --impure $argv";
 
   systemd.user.sessionVariables = {
     PATH = "$HOME/.nix-profile/bin:/nix/var/nix/profiles/default/bin:$PATH";
