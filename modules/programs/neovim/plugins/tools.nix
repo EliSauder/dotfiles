bbb6b4899e78aadc49a44814984be968710c2b25
EliSauder <24995216+EliSauder@users.noreply.github.com>
2026-09-22 11:07:10 -0700

    feat: add excel file support to neovim

--- modules/programs/neovim/plugins/tools.nix
+++ modules/programs/neovim/plugins/tools.nix
@@ -1,9 +1,45 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
 
   home.packages = [
     pkgs.gh
   ];
+
+  programs.nixvim.extraPlugins = [
+    (pkgs.vimUtils.buildVimPlugin {
+      name = "excel.nvim";
+      src = pkgs.fetchFromGitHub {
+        owner = "HimadriChakra12";
+        repo = "excel.nvim";
+        rev = "fc529b9cc18c4edf988749aa3238d1cb156f7720";
+        hash = "sha256-9hRM34JQpFjoPDBbW59Cs6w8gwsr/Lsi0HDjx+mozXA=";
+      };
+      buildInputs = [
+        pkgs.libreoffice
+      ];
+    })
+  ];
+  programs.nixvim.extraPackages = [
+    pkgs.libreoffice
+  ];
+  programs.nixvim.extraConfigLua =
+    let
+      py = pkgs.python3.withPackages (ps: [
+        ps.pandas
+        ps.openpyxl
+        ps.xlrd
+      ]);
+    in
+    ''
+      require("excel").setup({
+        max_col_width = 20,
+        min_col_width = 8,
+        show_gridlines = true,
+        auto_recalc = true,
+
+        python_cmd = "${py}/bin/python3",
+      })
+    '';
   programs.nixvim.plugins = {
     snacks = {
       enable = true;
